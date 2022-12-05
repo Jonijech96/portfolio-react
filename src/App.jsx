e55ad5c128e0ae615b55b5cdb80a047372f7fd4e
JoniJech <emanueljech.1996@gmail.com>
2022-12-05 18:16:39 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+import { useEffect, useState } from "react"
+import useMediaQuery from "./hooks/useMediaQuery";
+import DotGroup from "./scenes/DotGroup";
+import Navbar from "./scenes/Navbar";
+import Landing from "./scenes/Landing";
+import LineGradient from "./components/LineGradient";
+import MySkills from "./scenes/MySkills";
+
+function App() {
+  const [selectedPage, setSelectedPage] = useState('home');
+  const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
+  const [isTopOfPage, setIsTopOfPage] = useState(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setIsTopOfPage(true);
+        // setSelectedPage("home");
+      }
+      if (window.scrollY !== 0) setIsTopOfPage(false);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  return (
+    <div className="App bg-deep-blue">
+      <Navbar
+        isTopOfPage={isTopOfPage} selectedPage={selectedPage} setSelectedPage={setSelectedPage}
+      />
+      <div className="w-5/6 mx-auto md:h-full">
+        {isAboveMediumScreens && (
+          <DotGroup 
+          selectedPage={selectedPage}
+          setSelectedPage={setSelectedPage}
+          />
+        )}
+        <Landing setSelectedPage={setSelectedPage} />
+      </div>
+        <LineGradient />
+        <div className="w-5/6 mx-auto md:h-full">
+          <MySkills />
+        </div>
+    </div>
+  )
+}
+
+export default App
