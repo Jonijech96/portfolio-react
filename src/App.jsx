186357457ade794eca76807b9aad543dc6021136
JonathanChorolque <64989037+Jonijech96@users.noreply.github.com>
2024-03-26 20:25:10 -0300

    update app.jsx : setea por defecto valor 'inicio' en selectedPage y agrega funcionalidad de scroll active link

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import useMediaQuery from "./hooks/useMediaQuery";
 import DotGroup from "./scenes/DotGroup";
 import Navbar from "./scenes/Navbar";
@@ -11,10 +11,39 @@ import Footer from "./scenes/Footer";
 import AboutMe from "./scenes/AboutMe";
 
 function App() {
-  const [selectedPage, setSelectedPage] = useState("home");
+  const [selectedPage, setSelectedPage] = useState("inicio");
   const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
   const [isTopOfPage, setIsTopOfPage] = useState(true);
 
+  const targetRef = useRef(null);
+  const targetRef1 = useRef(null);
+  const targetRef2 = useRef(null);
+  const targetRef3 = useRef(null);
+  const targetRef4 = useRef(null);
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          setSelectedPage(entry.target.id)
+          // Aquí puedes realizar acciones cuando el elemento es visible
+        }
+      });
+    },{rootMargin:'-50% 0px -50% 0px'});
+
+    if (targetRef.current) observer.observe(targetRef.current);
+    if (targetRef1.current) observer.observe(targetRef1.current);
+    if (targetRef2.current) observer.observe(targetRef2.current);
+    if (targetRef3.current) observer.observe(targetRef3.current);
+    if (targetRef4.current) observer.observe(targetRef4.current);
+
+    return () => {
+      if (targetRef.current) observer.unobserve(targetRef.current);
+      if (targetRef1.current) observer.unobserve(targetRef1.current);
+      if (targetRef2.current) observer.unobserve(targetRef2.current);
+      if (targetRef3.current) observer.unobserve(targetRef3.current);
+      if (targetRef4.current) observer.unobserve(targetRef4.current);
+    };
+  }, []);
   useEffect(() => {
     const handleScroll = () => {
       if (window.scrollY === 0) {
@@ -33,7 +62,7 @@ function App() {
         selectedPage={selectedPage}
         setSelectedPage={setSelectedPage}
       />
-      <div className="w-5/6 mx-auto md:h-full min-h-screen">
+      <div ref={targetRef} id="inicio" className="w-5/6 mx-auto md:h-full min-h-screen">
         {isAboveMediumScreens && (
           <DotGroup
             selectedPage={selectedPage}
@@ -43,19 +72,19 @@ function App() {
         <Landing setSelectedPage={setSelectedPage} />
       </div>
       <LineGradient />
-      <div className="w-5/6 mx-auto md:h-full min-h-screen">
+      <div  ref={targetRef1} id="sobre-mi" className="w-5/6 mx-auto md:h-full min-h-screen">
         <AboutMe />
       </div>
       <LineGradient />
-      <div className="w-5/6 mx-auto md:h-full ">
+      <div ref={targetRef2} id="habilidades" className="w-5/6 mx-auto md:h-full ">
         <MySkills />
       </div>
       <LineGradient />
-      <div className="w-5/6 mx-auto">
+      <div ref={targetRef3} id="proyectos" className="w-5/6 mx-auto">
         <Projects />
       </div>
       <LineGradient />
-      <div className="w-5/6 mx-auto md:h-full">
+      <div ref={targetRef4} id="contacto" className="w-5/6 mx-auto md:h-full">
         <Contact />
       </div>
       <Footer />
